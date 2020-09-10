de677cc03b0c09a3f710593528533f86db3498bf
Naoki <poteto9270@g.ecc.u-tokyo.ac.jp>
2020-09-10 14:44:48 +0900

    プロローグでスタックを必要な分だけ伸ばすよう修正

--- main.c
+++ main.c
@@ -18,10 +18,11 @@ int main(int argc, char **argv) {
     printf("main:\n");
 
     // プロローグ
-    // 変数26個分の領域を確保する
+    // ローカル変数の領域を確保する
     printf("    push rbp\n");
     printf("    mov rbp, rsp\n");
-    printf("    sub rsp, 208\n");
+    if (locals)
+        printf("    sub rsp, %d\n", locals->offset);
 
     // 先頭の式から順にコード生成
     for (int i = 0; code[i]; i++) {
